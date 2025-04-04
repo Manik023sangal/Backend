b34e1da450caae08da72dd7fc30281308c82817b
Manik023sangal <maniksangal46@gmail.com>
2025-04-04 10:05:49 +0530

    middleware work

---
+++ Express/mid.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//user midd
+const adi = (req, res, next) => {
+    const age = req.query.age
+    if(!age){
+        res.send("please enter age")
+    }
+    if(age <= 18){
+        res.send("sorry your age is under 18")
+    }
+    next();
+}
+
+app.get("/", (req, res) => {
+    res.send("This is my home page")
+})
+
+//route middleware
+app.get("/about", adi, (req, res) => {
+    res.send("This is my about page");
+})
+
+app.listen(port, () => {
+    console.log(`app is running at port: ${port}`);
+})
